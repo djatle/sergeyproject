5935b347e2715cc17d8c6ac182524be2c6ba3ad4
Ofitserov <ofitserov.vladimir@gmail.com>
2015-12-16 09:19:03 +0300

    First com

---
+++ class/Database.class.php
@@ -0,0 +1,105 @@
+<?php
+
+Class Database {
+	
+private $host = DB_HOST;
+private $user = DB_USER;
+private $pass = DB_PASS;
+private $dbname = DB_NAME;
+private $port = DB_PORT;
+private $stmt;	
+private $dbh;
+private $error;	
+
+	
+	public function __construct(){
+		
+		$dsn = 'mysql:host=' . $this->host . ';port= '. $this->port.';dbname=' . $this->dbname ;        //установка типа подключаемой базы данных
+                 		
+		$options = array(
+          PDO::ATTR_PERSISTENT => true, 
+          PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION );                          //фиг знает, что здесь присходит
+				
+		
+	
+	    try {
+        $this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
+		    }
+		
+        catch (PDOException $e) {
+               $this->error = $e->getMessage();                                  //вроде как проиписаны методы обработки исключений
+            }	
+			
+	}
+	
+	public function query($query){
+          $this->stmt = $this->dbh->prepare($query);                          //метод для обработки ключевого слова query
+	        }
+    		
+    public function bind($param, $value, $type = null){
+    if (is_null($type)) {
+        switch (true) {
+            case is_int($value):
+                $type = PDO::PARAM_INT;
+                break;
+            case is_bool($value):
+                $type = PDO::PARAM_BOOL;
+                break;
+            case is_null($value):
+                $type = PDO::PARAM_NULL;
+                break;
+            default:
+                $type = PDO::PARAM_STR;
+        }
+    }
+    $this->stmt->bindValue($param, $value, $type);
+}
+
+    public function execute(){
+        return $this->stmt->execute();                                               // Всё это счастье
+                             }   
+							 
+	public function resultset(){
+    $this->execute();
+    return $this->stmt->fetchAll(PDO::FETCH_ASSOC);            
+                               }	
+
+    public function single(){
+    $this->execute();
+    return $this->stmt->fetch(PDO::FETCH_ASSOC);                                  // подготавливает
+                            }	
+
+    public function rowCount(){
+    return $this->stmt->rowCount();
+                              }	
+
+    public function lastInsertId(){
+    return $this->dbh->lastInsertId();
+                                  }
+								                                                 // набор основных методов
+	public function beginTransaction(){
+    return $this->dbh->beginTransaction();
+                                      }		
+
+    public function endTransaction(){
+    return $this->dbh->commit();
+                                    }		
+
+    public function cancelTransaction(){
+    return $this->dbh->rollBack();
+                                       }	
+
+    public function debugDumpParams(){
+    return $this->stmt->debugDumpParams();                                       // для работы с базой данных
+                                     }									   
+							  
+							 
+ 
+ 
+}
+ 
+            
+			
+			
+	
+	
---
+++ reg.php
@@ -0,0 +1,94 @@
+<?php require_once('templates/top.php');?>
+<?php require_once('class/Auth.php');?>
+
+
+
+
+
+<h2>Регистрация</h2>
+<?php
+
+
+
+      if ($_POST) 
+	  {
+		  $obj = new Auth( $_POST['Login'], $_POST['Email'], $_POST['Password'], $_POST['Passwordagain']);
+		  if(count($obj->err)>0){
+			  print_r($obj->err);
+		  }   else {
+			 $obj->regi();
+		  ?>
+          <script>
+            document.location.href = 'index.php';
+           </script>
+            <?php
+		  
+		  }        
+						   
+	  }
+/*	  
+		// echo"<pre>";
+		 //print_r($_POST);
+	  //echo"</pre>";
+	  //}
+	 // if (empty ($_POST['Email'])){
+	//	  $err[]= 'не указан email';
+	  
+	 // }
+	  
+	//  if (empty ($_POST['Login'])){
+	 // $err[]= 'не указан логин';}
+	 
+    //  if (empty ($_POST['Password'])){
+	//  $err[]= 'не указан пароль';} 
+
+   //   if ($_POST['Password']!=$_POST['Password_2']){
+   //   $err[]= 'несовпадают пароли';}
+	  
+	//  $query = "SELECT * FROM users WHERE login = '".$_POST['Login']."'";
+	//  $resp = mysql_query($query);             //
+	//  $row = mysql_fetch_array($resp);         //создали массив
+	//  $err[] = 'такой логин уже существует';}
+        	  
+		  
+ //     if (count($err)>0){
+ //         echo"<pre>";
+ //         print_r($err);
+ //         echo"</pre>";		  
+//	  }
+//	   else{
+//		   $query = "INSERT INTO users VALUES (Null, '".$_POST['Email']."', '".$_POST['Login']."', '".$_POST['Password']."', now(), now(), 'unblock')";
+		   //$err=$db->exec($query);
+//		   $res = mysql_query($query);
+           
+          ?>
+          <script>
+//            document.location.href = 'index.php';
+           </script>
+            <?php		   
+//	   }
+	  ?>
+	  */
+	  ?>
+<form method = "POST" action = 'reg.php'>
+ 
+  <div class="form-group">
+    <label for="exampleInputName2">Name</label>
+    <input type="text" class="form-control" id="exampleInputName2" placeholder="Jane Doe" name = "Login">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email" name="Email">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name='Password'>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Repeat Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="Passwordagain">
+  </div>
+  
+ 
+  <button type="submit" class="btn btn-default">Submit</button>
+</form>
